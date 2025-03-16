8fb69f892c790c37e7afe9793e5c47ac718fdfa2
ttelcl <9429819+ttelcl@users.noreply.github.com>
2025-03-16 22:45:26 +0200

    Added a stub WPF application

---
+++ LcLauncher/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace LcLauncher;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App: Application
+{
+}
+
