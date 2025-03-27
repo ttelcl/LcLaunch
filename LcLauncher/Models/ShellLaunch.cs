009850e7c1f7ce2bca93861c19036595a2bb1a54
ttelcl <9429819+ttelcl@users.noreply.github.com>
2025-03-27 23:11:18 +0200

    Adding more fields to ShellLaunch. Plus adding a typical JSON example.

--- LcLauncher/Models/ShellLaunch.cs
+++ LcLauncher/Models/ShellLaunch.cs
@@ -10,8 +10,31 @@ using System.Threading.Tasks;
 using System.Diagnostics;
 
 using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System.ComponentModel;
 
 namespace LcLauncher.Models;
+
+/* JSON examples:
+ * Note that this class is primarily for launching documents,
+ * not so much executables. Although those work too.
+ 
+{
+  "target": "c:\\Windows\\System32\\@WLOGO_48x48.png",
+  "tooltip": "Windows logo",
+  "title": "Logo image",
+  "windowStyle": "Normal",
+  "verb": "",
+  "arguments": []
+}
+
+ * Most fields are optional. Only 'target' is required.
+
+{
+  "target": "c:\\Windows\\System32\\@WLOGO_48x48.png"
+}
+
+*/
 
 /// <summary>
 /// Tile content for traditional application launcher,
@@ -22,11 +45,17 @@ public class ShellLaunch
   public ShellLaunch(
     string target,
     string? tooltip = null,
-    string? title = null)
+    string? title = null,
+    ProcessWindowStyle windowStyle = ProcessWindowStyle.Normal,
+    string verb = "",
+    IEnumerable<string>? arguments = null)
   {
     TargetPath = target;
     Tooltip = tooltip;
     Title = title;
+    WindowStyle = windowStyle;
+    Verb = verb;
+    Arguments = arguments?.ToList() ?? [];
   }
 
   [JsonProperty("target")]
@@ -40,4 +69,30 @@ public class ShellLaunch
   /// </summary>
   [JsonProperty("title", NullValueHandling = NullValueHandling.Ignore)]
   public string? Title { get; set; }
+
+  /// <summary>
+  /// The startup window style. Default is Normal. Other options are
+  /// Hidden, Minimized, and Maximized.
+  /// </summary>
+  [JsonProperty("windowStyle", DefaultValueHandling = DefaultValueHandling.Ignore)]
+  [JsonConverter(typeof(StringEnumConverter))]
+  [DefaultValue(ProcessWindowStyle.Normal)]
+  public ProcessWindowStyle WindowStyle { get; set; }
+
+  /// <summary>
+  /// The verb to use when launching the target. Default is empty (default action).
+  /// Rarely used, but available for unusual cases. Valid values depend on the
+  /// target's file type!
+  /// </summary>
+  [JsonProperty("verb", DefaultValueHandling = DefaultValueHandling.Ignore)]
+  [DefaultValue("")]
+  public string Verb { get; set; }
+
+  [JsonProperty("arguments")]
+  public List<string> Arguments { get; set; }
+
+  public bool ShouldSerializeArguments()
+  {
+    return Arguments.Count > 0;
+  }
 }
