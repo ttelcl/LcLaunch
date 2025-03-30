b0fc1dc89589d220e4c57d5044be61630738a62f
ttelcl <9429819+ttelcl@users.noreply.github.com>
2025-03-30 23:09:33 +0300

    Initial datastore and TileList. TileList isn't used yet.

---
+++ LcLauncher/Models/TileList.cs
@@ -0,0 +1,83 @@
+﻿/*
+ * (c) 2025  ttelcl / ttelcl
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LcLauncher.Storage;
+
+using Newtonsoft.Json;
+
+namespace LcLauncher.Models;
+
+/// <summary>
+/// An ordered list of tiles, together with an ID.
+/// This is not serialized direcly, but wraps the
+/// result or source of serialization.
+/// </summary>
+public class TileList
+{
+  /// <summary>
+  /// Create a new TileList. Use <see cref="Load"/> or
+  /// <see cref="Create(Guid?)"/> to call this constructor.
+  /// </summary>
+  private TileList(
+    Guid id,
+    IEnumerable<TileData?> tiles)
+  {
+    Id = id;
+    Tiles = tiles.ToList();
+  }
+
+  /// <summary>
+  /// Create a brand new empty TileList, optionally using
+  /// a predefined ID.
+  /// </summary>
+  /// <param name="id">
+  /// The optional ID to use. If null, a new ID is generated.
+  /// </param>
+  /// <returns></returns>
+  public static TileList Create(
+    Guid? id = null)
+  {
+    return new TileList(id ?? Guid.NewGuid(), []);
+  }
+
+  /// <summary>
+  /// Load a TileList from a *.tile-list file in the given folder.
+  /// </summary>
+  /// <param name="store">
+  /// The store in which to look for the file.
+  /// </param>
+  /// <param name="id">
+  /// The ID, implying the file name.
+  /// </param>
+  /// <returns>
+  /// Returns the list that was loaded, or null if the file
+  /// did not exist.
+  /// </returns>
+  public static TileList? Load(
+    LcLaunchDataStore store,
+    Guid id)
+  {
+    var list = store.LoadData<TileData?[]>(
+      id, ".tile-list");
+    return list == null ? null : new TileList(id, list);
+  }
+
+  public void Save(
+    LcLaunchDataStore store)
+  {
+    store.SaveData(Id.ToString(), ".tile-list", Tiles);
+  }
+
+  public Guid Id { get; }
+
+  public List<TileData?> Tiles { get; }
+}
---
+++ LcLauncher/Storage/LcLaunchDataStore.cs
@@ -0,0 +1,109 @@
+﻿/*
+ * (c) 2025  ttelcl / ttelcl
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+
+namespace LcLauncher.Storage;
+
+/// <summary>
+/// Database for persisting data for the Launcher.
+/// </summary>
+public class LcLaunchDataStore
+{
+  /// <summary>
+  /// Create a new LcLaunchDataStore and ensure the data folder exists.
+  /// </summary>
+  public LcLaunchDataStore(
+    string? dataFolder = null)
+  {
+    DataFolder = String.IsNullOrEmpty(dataFolder)
+      ? DefaultFolder
+      : Path.GetFullPath(dataFolder);
+    if(!Directory.Exists(DataFolder))
+    {
+      Directory.CreateDirectory(DataFolder);
+    }
+  }
+
+  public string DataFolder { get; }
+
+  public static string DefaultFolder { get; } = Path.Combine(
+    Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+    "LcLauncher");
+
+  public T? LoadData<T>(
+    string tag,
+    string extension) where T : class
+  {
+    var path = GetDataFileName(tag, extension);
+    if(!File.Exists(path))
+    {
+      return null;
+    }
+    return JsonConvert.DeserializeObject<T>(File.ReadAllText(path));
+  }
+
+  public T? LoadData<T>(
+    Guid id,
+    string extension) where T : class
+  {
+    return LoadData<T>(id.ToString(), extension);
+  }
+
+  public void SaveData<T>(
+    string tag,
+    string extension,
+    T data)
+  {
+    var path = GetDataFileName(tag, extension);
+    var json = JsonConvert.SerializeObject(data, Formatting.Indented);
+    var tmpPath = path + ".tmp";
+    File.WriteAllText(tmpPath, json);
+    if(File.Exists(path))
+    {
+      var bakName = path + ".bak";
+      if(File.Exists(bakName))
+      {
+        File.Delete(bakName);
+      }
+      File.Replace(tmpPath, path, bakName);
+    }
+    else
+    {
+      File.Move(tmpPath, path);
+    }
+  }
+
+  public void SaveData<T>(
+    Guid id,
+    string extension,
+    T data)
+  {
+    SaveData(id.ToString(), extension, data);
+  }
+
+  public string GetDataFileName(
+    string tag,
+    string extension)
+  {
+    if(tag.IndexOfAny(['/', '\\']) >= 0)
+    {
+      throw new ArgumentException("Tag must not contain directory separators.");
+    }
+    if(!extension.StartsWith('.'))
+    {
+      throw new ArgumentException("Extension must start with a period.");
+    }
+    return Path.Combine(DataFolder, tag + extension);
+  }
+
+}
